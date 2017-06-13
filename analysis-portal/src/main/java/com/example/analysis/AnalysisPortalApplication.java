f8dc916c4751d6042a3aaa04e5ff603bf051f473
puroc <471049931@qq.com>
2017-06-13 16:00:23 +0800

    1

--- analysis-portal/src/main/java/com/example/analysis/AnalysisPortalApplication.java
+++ analysis-portal/src/main/java/com/example/analysis/AnalysisPortalApplication.java
@@ -1,8 +1,13 @@
 package com.example.analysis;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan
+@SpringBootConfiguration
+@EnableAutoConfiguration
 @SpringBootApplication
 public class AnalysisPortalApplication {
 
